34fb74eb502cce9d84160114525263e1fb0ed82c
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-05-04 14:57:07 +1200

    Tuesday 04/05/21

---
+++ MyGame/Chicken.gd
@@ -0,0 +1,29 @@
+extends Area2D
+
+var moving = false
+export var SPEED = 100
+
+func _process(delta):
+	moving = false
+	if Input.is_action_pressed("ui_left"):
+		move(-SPEED, 0, delta, true)
+	if Input.is_action_pressed("ui_right"):
+		move(SPEED, 0, delta)
+	if Input.is_action_pressed("ui_up"):
+		move(0, -SPEED, delta)
+	if Input.is_action_pressed("ui_down"):
+		move(0, SPEED, delta)
+	
+	if moving:
+		$AnimationPlayer.play("ChickenWalk")
+	else:
+		$AnimationPlayer.play("BeginIdle")
+
+func move(xspeed, yspeed, delta, flip = false):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
+	moving = true
+	$Sprite.flip_h = flip
+
+func _ready():
+	pass
