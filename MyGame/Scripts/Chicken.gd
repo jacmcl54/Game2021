1e5812877e56c9d952dba3e2c856a4f63b5a09dd
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-05-12 14:57:58 +1200

    12/05/2021
    new movement code in the works

--- MyGame/Scripts/Chicken.gd
+++ MyGame/Scripts/Chicken.gd
@@ -1,30 +1,44 @@
-extends Area2D
+extends KinematicBody2D
 
-var moving = false
-export var SPEED = 100
+#var moving = false
+#export var SPEED = 75
+#
+#func _process(delta):
+#	moving = false
+#	if Input.is_action_pressed("ui_left"):
+#		move(-SPEED, 0, delta, true)
+#		$Sprite.flip_h = false
+#	if Input.is_action_pressed("ui_right"):
+#		move(SPEED, 0, delta)
+#		$Sprite.flip_h = true
+#	if Input.is_action_pressed("ui_up"):
+#		move(0, -SPEED, delta)
+#	if Input.is_action_pressed("ui_down"):
+#		move(0, SPEED, delta)
+#
+#	if moving:
+#		$AnimationPlayer.play("ChickenWalk")
+#	else:
+#		$AnimationPlayer.play("Idle")
+#
+#func move(xspeed, yspeed, delta, _flip = false):
+#	position.x += xspeed * delta
+#	position.y += yspeed * delta
+#	moving = true
+#
+#func _ready():
+#	pass
 
-func _process(delta):
-	moving = false
-	if Input.is_action_pressed("ui_left"):
-		move(-SPEED, 0, delta, true)
-		$Sprite.flip_h = false
-	if Input.is_action_pressed("ui_right"):
-		move(SPEED, 0, delta)
-		$Sprite.flip_h = true
-	if Input.is_action_pressed("ui_up"):
-		move(0, -SPEED, delta)
-	if Input.is_action_pressed("ui_down"):
-		move(0, SPEED, delta)
-	
-	if moving:
-		$AnimationPlayer.play("ChickenWalk")
-	else:
-		$AnimationPlayer.play("Idle")
+const UP = Vector2(0, -1)
 
-func move(xspeed, yspeed, delta, _flip = false):
-	position.x += xspeed * delta
-	position.y += yspeed * delta
-	moving = true
+var velocity = Vector2()
+var move_speed = 5 * 96
 
-func _ready():
-	pass
+
+func _physics_process(delta):
+		
+		move_and_slide(velocity, UP)
+
+func _get_input():
+	var move_direction = -int(Input.is_action_pressed("move_left")) + int(Input.is_action_pressed("move_right"))
+	velocity.x = lerp(velocity.x, move_speed * move_direction, 0.2)
