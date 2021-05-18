e53a10bd392f1684b96acc8b57554a6b032ec0d4
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-05-18 12:24:04 +1200

    18/05/2021

--- MyGame/Scripts/Chicken.gd
+++ MyGame/Scripts/Chicken.gd
@@ -33,31 +33,34 @@ const UP = Vector2(0, -1)
 const SLOPE_STOP = 64
 
 var velocity = Vector2()
-var move_speed = 75
-var gravity = 700
-var jump_velocty = -720
+var move_speed = 200
+var gravity = 900
+var jump_velocty = -600
 var is_grounded
 var anim_player
+var moving = false
 
 onready var raycasts = $Raycasts
 
 func _physics_process(delta):
 	_get_input()
 	velocity.y += gravity * delta
-	
 	velocity = move_and_slide(velocity, UP, SLOPE_STOP)
-	
 	is_grounded = _check_is_grounded()
+	if moving:
+		$AnimationPlayer.play("ChickenWalk")
+	else:
+		$AnimationPlayer.play("ChickenWalk")
 
 func _input(event):
 	if event.is_action_pressed("jump") && is_grounded:
 		velocity.y = jump_velocty
 
-func _get_input():
+func _get_input(_flip = false):
 	var move_direction = -int(Input.is_action_pressed("ui_left")) + int(Input.is_action_pressed("ui_right"))
 	velocity.x = lerp(velocity.x, move_speed * move_direction, _get_h_weight())
-	if move_direction != 0:
-		$Sprite.scale.x = move_direction
+	#if move_direction != 0:
+	#	$Sprite.scale.x = move_direction
 
 func _get_h_weight():
 	return 0.2 if is_grounded else 0.1
@@ -66,16 +69,9 @@ func _check_is_grounded():
 	for raycast in raycasts.get_children():
 		if raycast.is_colliding():
 			return true
-	
 	return false
 
-func _assign_animation():
-	var anim = "Idle"
-	
-	if !is_grounded:
-		anim = "ChickenWalk"
-	elif velocity.x != 0:
-		anim = "ChickenWalk"
-	
-	if anim_player.assigned_animation != anim:
-		anim_player.player(anim)
+func move(xspeed, yspeed, delta, _flip = false):
+	position.x += xspeed * delta
+	position.y += yspeed * delta
+	moving = true
