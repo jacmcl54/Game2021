4a0fa2f57fb0e028fdaa8627bf68c125252c21a6
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-06-23 13:21:25 +1200

    23/06/2021

--- MyGame/Scripts/Coin.gd
+++ MyGame/Scripts/Coin.gd
@@ -5,6 +5,7 @@ signal coin_collected
 func _on_Coin_body_entered(body):
 	$AnimationPlayer.play("CoinTaken")
 	emit_signal("coin_collected")
+	print("Coin collected")
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
--- MyGame/Scripts/CoinHUD.gd
+++ MyGame/Scripts/CoinHUD.gd
@@ -8,7 +8,8 @@ func _ready():
 func _physics_process(delta):
 	if coins == 3:
 		yield(get_tree().create_timer(0.5), "timeout")
-		get_tree().change_scene("res://Scenes/Level2.tscn")
+		get_tree().change_scene("res://Scenes/Level1.tscn")
+		print("Required coin amount reached, switching to next level")
 
 func _on_Coin_collected():
 	coins = coins + 1
