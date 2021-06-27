b98c100ff296fcfdbbb8ef4caccd41d104efc88a
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-06-28 09:43:22 +1200

    28/06/2021
    coinhud has its own scene and theres now thousands of errors!

--- MyGame/Scripts/CoinHUD.gd
+++ MyGame/Scripts/CoinHUD.gd
@@ -6,10 +6,16 @@ func _ready():
 	$CoinNumber.text = String(coins)
 
 func _physics_process(delta):
-	if coins == 3:
-		yield(get_tree().create_timer(0.5), "timeout")
-		get_tree().change_scene("res://Scenes/Level1.tscn")
+	if get_tree().current_scene.name == "Level1":
+		if coins == 3:
+			yield(get_tree().create_timer(0.5), "timeout")
+			get_tree().change_scene("res://Scenes/Level2.tscn")
+	if get_tree().current_scene.name == "Level2":
+		if coins == 1:
+			yield(get_tree().create_timer(0.5), "timeout")
+			get_tree().change_scene("res://Scenes/Level1.tscn")
 
 func _on_coin_collected():
 	coins = coins + 1
 	_ready()
+
