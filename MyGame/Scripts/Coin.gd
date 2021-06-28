cad8dbf0f94fb8a4866fc5e5bafcbc64a2d7a5e1
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-06-29 10:56:47 +1200

    29/06/2021

--- MyGame/Scripts/Coin.gd
+++ MyGame/Scripts/Coin.gd
@@ -6,6 +6,7 @@ func _on_Coin_body_entered(body):
 	$AnimationPlayer.play("CoinTaken")
 	emit_signal("coin_collected")
 	print("Coin collected")
+	set_collision_mask_bit(0, false)
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
