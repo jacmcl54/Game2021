25947dfbded405dcba92289fe4efff8f830a7087
jacmcl54 <jacmcl54@kingshigh.school.nz>
2021-07-01 13:23:49 +1200

    01/07/2021

--- MyGame/Scripts/Enemy.gd
+++ MyGame/Scripts/Enemy.gd
@@ -2,17 +2,20 @@ extends KinematicBody2D
 
 var velocity = Vector2()
 export var direction = -1
+export var detects_cliffs = true
 
 func _ready():
 	if direction == 1:
 		$AnimatedSprite.flip_h = true
-
+	$FloorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
+	$FloorChecker.enabled = detects_cliffs
 
 func _physics_process(delta):
 	
-	if is_on_wall():
+	if is_on_wall() or not $FloorChecker.is_colliding() and detects_cliffs and is_on_floor():
 		direction = direction * -1
 		$AnimatedSprite.flip_h = not $AnimatedSprite.flip_h
+		$FloorChecker.position.x = $CollisionShape2D.shape.get_extents().x * direction
 	
 	velocity.y += 20
 	
@@ -20,4 +23,4 @@ func _physics_process(delta):
 	
 	velocity = move_and_slide(velocity, Vector2.UP)
 
-#VIDEO AT 28:27
+#VIDEO AT 49:35
