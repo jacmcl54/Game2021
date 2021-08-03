90a2a704f18c83362aa95243cffd23ef2b4a10d6
Jack McLeod <jacmcl54@kingshigh.school.nz>
2021-08-03 14:56:57 +1200

    03/08/2021
    dialogue box code with .json file

--- MyGame/Scripts/SignDialog.gd
+++ MyGame/Scripts/SignDialog.gd
@@ -9,6 +9,19 @@ func _ready():
 	
 func play():
 	dialogues = load_dialogue()
+	$NinePatchRect/Name.text = dialogues[0]['name']
+	$NinePatchRect/Message.text = dialogues[0]['text']
+#	$NinePatchRect/Name.text = ("HOW TO PLAY")
+#	$NinePatchRect/Message.text = ("W, Up Arrow or Space - Jump\nA,D or Left/Right arrow keys to move")
+
+func _input(event):
+	if event.is_action_pressed("dialogue_interact"):
+		next_line()
+
+func next_line():
+#	$NinePatchRect/Message.text = ("Collect all the coins to get to the next level")
+	$NinePatchRect/Name.text = dialogues[1]['name']
+	$NinePatchRect/Message.text = dialogues[1]['text']
 
 func load_dialogue():
 	var file = File.new()
@@ -16,4 +29,5 @@ func load_dialogue():
 		file.open(dialogue_file, file.READ)
 		return parse_json(file.get_as_text())
 
-#Script at 2:50
+
+#video at 3:37
