264a06a25ce7327a6e0200416bb8d849edf9a3bb
Jack McLeod <jacmcl54@kingshigh.school.nz>
2021-08-16 10:56:27 +1200

    16/08/2021

--- MyGame/Scripts/CoinHUD.gd
+++ MyGame/Scripts/CoinHUD.gd
@@ -10,9 +10,7 @@ func _physics_process(delta):
 		OS.set_window_title("MyGame - Level 1")
 		if coins == 6:
 			yield(get_tree().create_timer(0.5), "timeout")
-			get_tree().reload_current_scene()
-#			get_tree().change_scene("res://Scenes/Level2.tscn")
-			print("3/3 coins collected, swtiching to next level.")
+			get_tree().change_scene("res://Scenes/Level2.tscn")
 			OS.alert("Congratulations, you passed the first stage!")
 	if get_tree().current_scene.name == "Level2":
 		OS.set_window_title("MyGame - Level 2")
