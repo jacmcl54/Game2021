5f79e244f22f70fe70658ec5117c5edf976abc56
Jack McLeod <jacmcl54@kingshigh.school.nz>
2021-11-02 14:56:57 +1300

    2/11/2021

--- MyGame/Scripts/Coin.gd
+++ MyGame/Scripts/Coin.gd
@@ -6,6 +6,7 @@ func _on_Coin_body_entered(body):
 	$AnimationPlayer.play("CoinTaken")
 	emit_signal("coin_collected")
 	set_collision_mask_bit(0, false)
+	$CoinSound.play()
 
 func _on_AnimationPlayer_animation_finished(anim_name):
 	queue_free()
